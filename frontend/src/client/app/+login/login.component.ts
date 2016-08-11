d9e7e00bf2cc23e6528eced2ab9549c253cbfcc5
kael <yorts52@qq.com>
2016-08-11 16:09:34 +0800

    添加登录

---
+++ frontend/src/client/app/+login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit }  from '@angular/core';
+import { FormGroup, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+
+import { FieldBase } from '../+field/field-base';
+import { FieldComponent } from '../+field/field.component';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'app/+login/login.component.html',
+  styleUrls: ['app/+login/login.component.css'],
+  directives: [FieldComponent, REACTIVE_FORM_DIRECTIVES],
+  providers:  [LoginService]
+})
+
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  logined = false;
+  fields: FieldBase<any>[] = [];
+
+  constructor(private rs: LoginService) {
+    this.fields = rs.getFields();
+  }
+
+  ngOnInit() {
+    this.form = this.rs.toFormGroup(this.fields);
+  }
+
+  showPassword () {
+    this.fields.forEach(field => {
+      if (field.key === 'password') {
+        field.type = field.type === 'password' ? 'text' : 'password';
+      }
+    });
+  }
+
+  login() {
+    this.rs
+      .login(this.form.value)
+      .subscribe(res => {
+        let body = res.json();
+        if (body && body.success) {
+          this.logined = true;
+        }
+      }, error => {
+        console.error(error);
+      });
+  }
+}
---
+++ frontend/src/client/app/+login/login.service.ts
@@ -0,0 +1,71 @@
+import { Injectable }       from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+
+import { FieldBase }     from '../+field/field-base';
+import { FieldText }     from '../+field/field-text';
+import { FieldRadio }     from '../+field/field-radio';
+import { FieldValidators } from '../+field/field-validators';
+
+class User {
+  constructor(
+    public username: string,
+    public password: string
+  ) { }
+}
+
+@Injectable()
+export class LoginService {
+
+  constructor(private http: Http) { }
+
+  private add_user_url = 'http://localhost:8100/login';
+
+  getFields() {
+    let fields: FieldBase<any>[] = [
+      new FieldText({
+        key: 'username',
+        label: '用户名',
+        value: '',
+        required: true,
+        pattern: 'username',
+        order: 1
+      }),
+      new FieldText({
+        key: 'password',
+        label: '密码',
+        type: 'password',
+        value: '',
+        required: true,
+        pattern: 'password',
+        order: 2
+      }),
+    ];
+    return fields.sort((a, b) => a.order - b.order);
+  }
+
+  toFormGroup(fields: FieldBase<any>[]) {
+    let group: any = {};
+
+    fields.forEach(field => {
+      group[field.key] =
+        field.pattern ?
+          new FormControl(field.value || '', FieldValidators[field.pattern]) :
+          field.required ?
+            new FormControl(field.value || '', Validators.required) :
+            new FormControl(field.value || '');
+    });
+    return new FormGroup(group);
+  }
+
+  login(data: Object) {
+    let body = JSON.stringify(data);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http
+      .post(this.add_user_url, body, { headers });
+  }
+}
